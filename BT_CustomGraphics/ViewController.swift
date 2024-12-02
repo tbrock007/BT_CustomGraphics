9be1eaaca0cb9cd603d76f906712bfdef13be484
tbrock007 <tbrock209@gmail.com>
2024-12-02 13:40:06 -0700

    Initial Commit

---
+++ BT_CustomGraphics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BT_CustomGraphics
+//
+//  Created by Brock Terry on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BT_CustomGraphicsTests/BT_CustomGraphicsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BT_CustomGraphicsTests.swift
+//  BT_CustomGraphicsTests
+//
+//  Created by Brock Terry on 12/2/24.
+//
+
+import Testing
+@testable import BT_CustomGraphics
+
+struct BT_CustomGraphicsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
